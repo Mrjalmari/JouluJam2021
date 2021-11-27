1b028464e4bdb7c103a7dc3137cb064ccc6294fc
atzka <attehilander@gmail.com>
2021-11-28 00:09:46 +0200

    Characters animations

---
+++ KamkXmasGameJam2021/Assets/Script/Ai/AiAttack.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiAttack : MonoBehaviour
+{
+    [SerializeField]
+    private float meleeAttackRange = 1;
+    [SerializeField]
+    private float rangedAttackHeight = 0;
+
+    [SerializeField]
+    private float directionMod;
+
+    [SerializeField]
+    private CharacterAttack characterAttack;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.right * directionMod;
+
+        if(Physics.Raycast(transform.position, direction, out RaycastHit raycastHit))
+        {
+            float distance = Vector3.Distance(transform.position, raycastHit.point);
+            if (raycastHit.collider.CompareTag("Character") && distance < meleeAttackRange)
+            {
+                characterAttack.MeleeAttack();
+            }
+            else if (raycastHit.collider.CompareTag("Character")) characterAttack.RangedAttack();
+
+        }
+
+    }
+}
